949c438fb43247cc02d1df4131b6a6b48d9d4386
LEARNET\william.jutbring <william.jutbring@elev.ga.lbs.se>
2018-10-05 15:55:50 +0200

    Ship + Trail added
    shipped

---
+++ Assets/shipscript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipscript : MonoBehaviour
+{
+    public float shipSpeed;
+    public float shipTurn;
+    public Color turnRightColor;
+    public Color turnLeftColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        shipSpeed = 0.15f;
+        shipTurn = 4f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // roterar z-axeln medsols
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate( 0, 0, -shipTurn);
+        }
+        // roterar z-axeln motsols
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate( 0, 0, shipTurn);
+        }
+        // kör skeppet hälften av normal hastighet...
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, shipSpeed/2, 0);
+        }
+        // ... eller kör skeppet normal hastighet
+        else
+        {
+            transform.Translate(0, shipSpeed, 0);
+        }
+    }
+}
